352becf719c235dc987a6566b08caf7657477afe
Xananax <xananax@yelostudio.com>
2013-10-22 16:25:16 +0300

    made menu stick only after first page

--- src/_main.js
+++ src/_main.js
@@ -1,76 +1,86 @@
-var scrollElement = 'body', $scrollElement
-var $pages = $('.page');
-var $window = $(window);
-var $body = $('body');
-var $menuLinks = $('a.menu-link')
-var resizeTimer;
+(function(){
+	var scrollElement = 'body', $scrollElement
+	var $pages = $('.page');
+	var $window = $(window);
+	var $body = $('body');
+	var $menuLinks = $('a.menu-link')
+	var $menu = $('#Navigation')
+	var resizeTimer;
 
-// determine scrolling element
-$('html, body').each(function(){
-	var $this = $(this);
-	var initScrollTop = $this.attr('scrollTop');
-	$this.attr('scrollTop', initScrollTop + 1);
-	if($this.attr('scrollTop') == initScrollTop + 1){
-		scrollElement = this.nodeName.toLowerCase();
-		$this.attr('scrollTop', initScrollTop);
-		return false;
-	}    
-});
-
-$scrollElement = $(scrollElement);
-
-function isScrolledIntoView($elem,offset,limit){
-
-	var docViewTop = $window.scrollTop();
-	var docViewBottom = docViewTop + $window.height();
-
-	var elemTop = $elem.offset().top;
-	if(offset===true){
-		var elemBottom = elemTop + $elem.height();
-	}
-	else{
-		elemBottom = elemTop + (offset ? offset : 2);
-	}
-
-	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop) && (elemTop <= docViewTop + (limit? limit : 100)));
-}
-
-function setPage(id){
-	var hash = '#'+id;
-	var $menuLink = $(hash+"-Link").addClass('active')
-	window.location.hash = hash;
-	$menuLinks.not($menuLink).removeClass('active');
-}
-
-$menuLinks.add('a.scrollDown').click(function(event){
-	var hash = $(this).attr('href');
-	var $target = $(hash);
-	if(!$target.length){return false;}
-	$scrollElement.stop().animate({scrollTop: $target.offset().top}, 500, 'swing', function() {window.location.hash = hash;})
-	event.preventDefault();
-})
-
-/* Force snap to panel on resize. */
-$window.resize(function() {
-	window.clearTimeout(resizeTimer);
-	resizeTimer = window.setTimeout(function() {
-		var hash = window.location.hash ? window.location.hash : '#Home';
-		$scrollElement.stop().animate({scrollTop: $(hash).offset().top}, 200);
-	}, 100);
-});
-
-$window.scroll(function(){
-	var x = $scrollElement.scrollTop();
-	var pos = parseInt(-x / 10);
-	$pages.css('background-position', '0% ' +  pos + 'px').each(function(){
-		var $page = $(this);
-		if(isScrolledIntoView($page)){
-			setPage(this.id,100);
+	// determine scrolling element
+	$('html, body').each(function(){
+		var $this = $(this);
+		var initScrollTop = $this.attr('scrollTop');
+		$this.attr('scrollTop', initScrollTop + 1);
+		if($this.attr('scrollTop') == initScrollTop + 1){
+			scrollElement = this.nodeName.toLowerCase();
+			$this.attr('scrollTop', initScrollTop);
 			return false;
-		}
-	})
-});
+		}    
+	});
 
-$('#Menu-Revealer').on('click',function(){
-	$body.toggleClass('menu-open')
-})
+	$scrollElement = $(scrollElement);
+
+	function isScrolledIntoView($elem,offset,limit){
+
+		var docViewTop = $window.scrollTop();
+		var docViewBottom = docViewTop + $window.height();
+
+		var elemTop = $elem.offset().top;
+		if(offset===true){
+			var elemBottom = elemTop + $elem.height();
+		}
+		else{
+			elemBottom = elemTop + (offset ? offset : 2);
+		}
+
+		return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop) && (elemTop <= docViewTop + (limit? limit : 100)));
+	}
+
+	function setPage($page,id,time){
+		if(id == 'Home'){
+			$menu.removeClass('fixed');
+		}else{
+			$menu.addClass('fixed');
+		}
+		var hash = '#'+id;
+		var $menuLink = $(hash+"-Link").addClass('active')
+		$menuLinks.not($menuLink).removeClass('active');
+		$scrollElement.stop().animate({scrollTop: $page.offset().top}, time || 0, 'swing', function() {window.location.hash = hash;})
+	}
+
+	$menuLinks.add('a.scrollDown').click(function(event){
+		var hash = $(this).attr('href');
+		var id = hash.replace('#','');
+		var $target = $(hash);
+		if(!$target.length){return false;}
+		setPage($target,id,500)
+		event.preventDefault();
+	})
+
+	/* Force snap to panel on resize. */
+	$window.resize(function() {
+		window.clearTimeout(resizeTimer);
+		resizeTimer = window.setTimeout(function() {
+			var hash = window.location.hash ? window.location.hash : '#Home';
+			var id = hash.replace('#','');
+			setPage($(hash),id,200)
+		}, 100);
+	});
+
+	$window.scroll(function(){
+		var x = $scrollElement.scrollTop();
+		var pos = parseInt(-x / 10);
+		$pages.css('background-position', '0% ' +  pos + 'px').each(function(){
+			var $page = $(this);
+			if(isScrolledIntoView($page)){
+				setPage($page,this.id);
+				return false;
+			}
+		})
+	});
+
+	$('#Menu-Revealer').on('click',function(){
+		$body.toggleClass('menu-open')
+	})
+})()
